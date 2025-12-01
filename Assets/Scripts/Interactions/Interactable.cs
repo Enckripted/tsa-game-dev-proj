fb88a6f379395b13c6915c020950600f59bdea3a
Enckripted <diegoemorales1@gmail.com>
2025-11-30 22:14:56 -0600

    interaction tweak
    holding down interact doesn't lead to multiple interactions any more

--- Assets/Scripts/Interactions/Interactable.cs
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -4,7 +4,6 @@ using UnityEngine.Events;
 public class Interactable : MonoBehaviour
 {
     [field: SerializeField] public string interactText { get; private set; } = "[E] Interact";
-    [field: SerializeField] public float interactTime { get; private set; } = 0.4f;
     public UnityEvent interactEvent { get; private set; }
 
     public void Interact()
