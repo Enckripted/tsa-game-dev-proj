16e3b69cb41a883d2d12916f3e170b4d0a2605f7
Nokhol <nikhilbob2009@gmail.com>
2026-01-22 19:54:02 -0600

    buncha art
    more art

--- Assets/Scripts/Conveyer Belt/ConveyerBelt.cs
+++ Assets/Scripts/Conveyer Belt/ConveyerBelt.cs
@@ -105,7 +105,9 @@ public class ConveyerBelt : MonoBehaviour
         GearItem chosenGear = RandomItemFactory.instance.createRandomItem(); // singleton call
 
         //parent under this object to tidy up the inspector
-        GameObject obj = Instantiate(droppedItemPrefab, startPos, Quaternion.identity, transform.parent); // create obj
+        GameObject obj = Instantiate(droppedItemPrefab, new Vector3(startPos.x, startPos.y, -4f), Quaternion.identity, transform.parent); // create obj
+
+        obj.transform.localScale = Vector3.one * 0.75f; // make smaller
 
         obj.SetActive(false); // prevent null refs in awake
 
