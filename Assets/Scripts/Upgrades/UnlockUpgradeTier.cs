83596818d12e5665021303deb529e85c0452f8e9
Enckripted <diegoemorales1@gmail.com>
2026-01-24 17:04:31 -0600

    added in upgrades for buying machines

---
+++ Assets/Scripts/Upgrades/UnlockUpgradeTier.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct UnlockUpgradeTier
+{
+    public double Cost;
+    public GameObject ObjectUnlock;
+}
---
+++ Assets/Scripts/Upgrades/UnlockUpgradeUi.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockUpgradeUi : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI costText;
+    [SerializeField] private TextMeshProUGUI tierText;
+    [SerializeField] private Button buyButton;
+
+    public UnlockUpgrade Upgrade;
+
+    void Start()
+    {
+        nameText.text = Upgrade.Name;
+        buyButton.onClick.AddListener(Upgrade.BuyUpgrade);
+    }
+
+    void Update()
+    {
+        costText.text = $"${Upgrade.GetCurrentTier().Cost:0.00}";
+        tierText.text = $"Tier: {Upgrade.CurrentTier}/{Upgrade.Tiers.Count}";
+    }
+}
